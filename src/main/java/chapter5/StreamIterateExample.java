d94ce94d565fbe9066425bde904de5335be7dfb7
Qi.Zhang4 <Qi.Zhang4@geely.com>
2020-08-27 08:52:03 +0800

    [ADD]增加5.7.4节通过迭代创建无限流的示例

---
+++ src/main/java/chapter5/StreamIterateExample.java
@@ -0,0 +1,18 @@
+package chapter5;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Qi.Zhang4
+ * @title: StreamIterateExample
+ * @projectName Java8InAction
+ * @description: TODO
+ * @date 2020/8/27 8:41
+ */
+public class StreamIterateExample {
+    public static void main(String[] args) {
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+    }
+}
