b970a299ffcbd41ad1a4d6231901671ab78a2b0d
Qi.Zhang4 <Qi.Zhang4@geely.com>
2020-08-30 18:29:00 +0800

    [ADD]增加第一章示例

--- src/main/java/chapter1/TransactionFilterByCurrencies.java
+++ src/main/java/chapter1/TransactionFilterByCurrencies.java
@@ -1,9 +1,6 @@
 package chapter1;
 
-import java.util.ArrayList;
-import java.util.Currency;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.stream.Collectors.groupingBy;
 
@@ -16,10 +13,29 @@ public class TransactionFilterByCurrencies {
     public static void main(String[] args) {
         List<Transaction> transactions = new ArrayList<>();
 
-        Transaction a = new Transaction(2000, Currency.getInstance());
+        Transaction a = new Transaction(2000, Currency.getInstance(Locale.CHINA));
+        Transaction a1 = new Transaction(1000, Currency.getInstance(Locale.CHINA));
+        Transaction b = new Transaction(2000, Currency.getInstance(Locale.UK));
+        Transaction b1 = new Transaction(500, Currency.getInstance(Locale.UK));
+        Transaction c = new Transaction(2000, Currency.getInstance(Locale.FRANCE));
+        Transaction c1 = new Transaction(5000, Currency.getInstance(Locale.FRANCE));
+        Transaction d = new Transaction(2000, Currency.getInstance(Locale.US));
+        Transaction d1 = new Transaction(1000, Currency.getInstance(Locale.US));
+
+        transactions.add(a);
+        transactions.add(a1);
+        transactions.add(b);
+        transactions.add(b1);
+        transactions.add(c);
+        transactions.add(c1);
+        transactions.add(d);
+        transactions.add(d1);
+
 
         Map<Currency,List<Transaction>> transactionsByCurrencies = transactions.stream()
                 .filter((Transaction t) -> t.getPrice() > 1000)
                 .collect(groupingBy(Transaction::getCurrency));
+
+        System.out.println();
     }
 }
